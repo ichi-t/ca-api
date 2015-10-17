b609070382c11a016b792e5930690bd56796f17a
ichi <ichi@ichi-MBP.local>
2015-10-17 17:46:05 +0900

    [add] controller

--- app/controllers/get_order_controller.rb
+++ app/controllers/get_order_controller.rb
@@ -2,8 +2,6 @@ class GetOrderController < ApplicationController
   def  index
     @request.unshift request:true if @request = Order.where(orderId: params[:orderid])
     
-    # @data = []
-
     render json: @request
     
   end
