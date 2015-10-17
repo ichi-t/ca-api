b55b9f827223e2868d88bb1e07c1009421352fbc
ichi <ichi@ichi-MBP.local>
2015-10-17 17:26:13 +0900

    [modify]

--- app/controllers/get_order_controller.rb
+++ app/controllers/get_order_controller.rb
@@ -1,14 +1,10 @@
 class GetOrderController < ApplicationController
   def  index
-    @request = Order.where(orderId: params[:orderid])
-    # Jbuilder.encode do |json|
-    #   render json.my_order @request
-    # end
+    @request.unshift request:true if @request = Order.where(orderId: params[:orderid])
+    
+    # @data = []
 
     render json: @request
-
-    # render :json => @request
-           
-    # render :text => "testing.."
+    
   end
 end
