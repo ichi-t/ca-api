6f56205ff88d2dd5d42dd0face1335376cf253a1
ichi <ichi@ichi-MBP.local>
2015-10-17 16:00:42 +0900

    tutorial

--- app/controllers/get_order_controller.rb
+++ app/controllers/get_order_controller.rb
@@ -1,4 +1,14 @@
 class GetOrderController < ApplicationController
-  
+  def  index
+    @request = Order.where(orderId: params[:orderid])
+    # Jbuilder.encode do |json|
+    #   render json.my_order @request
+    # end
 
+    render json: @request
+
+    # render :json => @request
+           
+    # render :text => "testing.."
+  end
 end
---
+++ app/views/get_order/index.json.jbuilder
@@ -0,0 +1,3 @@
+json.name "Orders"
+# json.count @users.count
+
